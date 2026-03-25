bd8359eba3d876df534d02ce18fd806326d59434
Pawel Bielecki <bieleckiltd@outlook.com>
2026-03-25 21:36:04 +0000

    Remove obsolete test and snippet YAML files for ESP32 and ESP8266 configurations in the esphome-jk-bms project

--- Docs/esphome-jk-bms-main/test-esp32.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $2 == tests/* ]]; then
-  C="../components"
-else
-  C="components"
-fi
-
-esphome -s mac_address 20:22:09:27:38:60 -s external_components_source $C ${1:-run} ${2:-esp32-example-faker.yaml}
